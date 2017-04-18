98a38f74d339c471e296d4b59d7075ae1a674484
yowcow <yowcow@cpan.org>
2017-04-18 12:14:45 +0900

    comment

--- test/recursion_test.erl
+++ test/recursion_test.erl
@@ -2,6 +2,7 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+%% fac()
 fac(0) -> 1;
 fac(N) when N > 0 -> N * fac(N - 1).
 
@@ -12,6 +13,7 @@ fac_test() ->
     ?assertEqual(6, fac(3)),
     ?assertEqual(24, fac(4)).
 
+%% len()
 len([]) -> 0;
 len([_ | Rest]) -> 1 + len(Rest).
 
@@ -20,6 +22,7 @@ len_test() ->
     ?assertEqual(1, len([1])),
     ?assertEqual(2, len([1, 1])).
 
+%% tail_fac()
 tail_fac(N) -> tail_fac(N, 1).
 
 tail_fac(0, Acc) -> Acc;
@@ -32,6 +35,7 @@ tail_fac_test() ->
     ?assertEqual(6, tail_fac(3)),
     ?assertEqual(24, tail_fac(4)).
 
+%% tail_len
 tail_len(L) -> tail_len(L, 0).
 
 tail_len([], Acc) -> Acc;
@@ -42,6 +46,7 @@ tail_len_test() ->
     ?assertEqual(1, tail_len([1])),
     ?assertEqual(2, tail_len([1, 1])).
 
+%% duplicate()
 duplicate(0, _) -> [];
 duplicate(N, T) when N > 0 ->
     [T | duplicate(N - 1, T)].
@@ -50,6 +55,7 @@ duplicate_test() ->
     ?assertEqual([], duplicate(0, hoge)),
     ?assertEqual([hoge, hoge], duplicate(2, hoge)).
 
+%% tail_duplicate()
 tail_duplicate(N, T) -> tail_duplicate(N, T, []).
 
 tail_duplicate(0, _, Acc) -> Acc;
@@ -59,6 +65,7 @@ tail_duplicate_test() ->
     ?assertEqual([], tail_duplicate(0, hoge)),
     ?assertEqual([hoge, hoge], tail_duplicate(2, hoge)).
 
+%% reverse()
 reverse([]) -> [];
 reverse([H | T]) -> reverse(T) ++ [H].
 
@@ -66,6 +73,7 @@ reverse_test() ->
     ?assertEqual([], reverse([])),
     ?assertEqual([3,2,1], reverse([1,2,3])).
 
+%% tail_reverse
 tail_reverse(L) -> tail_reverse(L, []).
 
 tail_reverse([], Acc) -> Acc;
