e78dc6d7ce29cdb33204a52a3a8794ff82f703c3
yowcow <yowcow@cpan.org>
2019-05-08 20:01:40 +0900

    binary

--- test/binary_test.erl
+++ test/binary_test.erl
@@ -11,3 +11,46 @@ binary_conversion_test_() ->
     [   ?_assertEqual(2, R1),
         ?_assertEqual(61, G1),
         ?_assertEqual(20, B1)].
+
+chunks(Bin) ->
+    chunks(Bin, []).
+
+chunks(<<H:8/binary, T/binary>>, Acc) ->
+    chunks(T, [H | Acc]);
+chunks(H, Acc) ->
+    [H | Acc].
+
+chunks_test() ->
+    {ok, Bin} = file:read_file("data/text.txt"),
+    Chunks = chunks(Bin),
+    Expected = [
+        <<10>>,
+        <<132,161,227,129,132,227,128,130>>,
+        <<175,227,129,190,227,129,160,231>>,
+        <<229,144,141,229,137,141,227,129>>,
+        <<129,130,227,130,139,227,128,130>>,
+        <<175,231,140,171,227,129,167,227>>,
+        <<229,144,190,232,188,169,227,129>>
+    ],
+    ?assertEqual(Expected, Chunks).
+
+combine(Chunks) ->
+    combine(Chunks, <<>>).
+
+combine([], Acc) -> Acc;
+combine([Chunk | T], Acc) ->
+    combine(T, <<Chunk/binary, Acc/binary>>).
+
+combine_test() ->
+    Chunks = [
+        <<10>>,
+        <<132,161,227,129,132,227,128,130>>,
+        <<175,227,129,190,227,129,160,231>>,
+        <<229,144,141,229,137,141,227,129>>,
+        <<129,130,227,130,139,227,128,130>>,
+        <<175,231,140,171,227,129,167,227>>,
+        <<229,144,190,232,188,169,227,129>>
+    ],
+    Bin = combine(Chunks),
+    Expected = <<"吾輩は猫である。名前はまだ無い。\n"/utf8>>,
+    ?assertEqual(Expected, Bin).
